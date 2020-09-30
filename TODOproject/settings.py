e96477ff9ea5abb2f50ed5507ac145ebc1d9774c
antolabarca <anto.labarca.s@gmail.com>
2020-09-30 19:14:04 -0300

    borrar db, agregar users

--- TODOproject/settings.py
+++ TODOproject/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'categorias',
 ]
 
+AUTH_USER_MODEL = 'todoapp.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
